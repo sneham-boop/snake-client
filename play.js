0dc1515ef708295f000f168da2e17d0c9ea82c32
Sneha Mahajan <sneh.km@gmail.com>
2022-04-18 17:15:51 -0400

    Add separate input.js file for accepting key strokes.

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
 setupInput();
